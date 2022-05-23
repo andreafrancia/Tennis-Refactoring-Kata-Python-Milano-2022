86e290446268c8b3a6a6d7d88afbfb96bb2a3937
Andrea Francia <andreafrancia@users.noreply.github.com>
2022-05-23 20:04:47 +0200

    Refactor

--- tennis.py
+++ tennis.py
@@ -7,7 +7,7 @@ class TennisGame1:
         self.player2_name = player2_name
         self.player1_points = 0
         self.player2_points = 0
-        
+
     def won_point(self, player_name):
         if player_name == self.player1_name:
             self.player1_points += 1
@@ -16,19 +16,23 @@ class TennisGame1:
 
     def score(self):
         score = Score(self.player1_points, self.player2_points)
+        if score.is_love_all():
+            return "Love-All"
+        if score.is_fifteen_all():
+            return "Fifteen-All"
+        if score.is_thirty_all():
+            return "Thirty-All"
         if score.players_have_tied_score():
-            return {
-                0: "Love-All",
-                1: "Fifteen-All",
-                2: "Thirty-All",
-            }.get(self.player1_points, "Deuce")
-        elif score.almost_one_of_the_players_is_winning():
-            if score.advantage_for_player_1():
-                return "Advantage " + self.player1_name
-            if score.advantage_for_player_2():
-                return "Advantage " + self.player2_name
-            if score.player1_won():
-                return "Win for " + self.player1_name
+            return "Deuce"
+
+        if score.advantage_for_player_1():
+            return "Advantage " + self.player1_name
+        if score.advantage_for_player_2():
+            return "Advantage " + self.player2_name
+        if score.player1_won():
+            return "Win for " + self.player1_name
+
+        if score.almost_one_of_the_players_is_winning():
             if score.player2_won():
                 return "Win for " + self.player2_name
         else:
@@ -57,14 +61,25 @@ class Score:
         return self.player1_points >= 4 or self.player2_points >= 4
 
     def advantage_for_player_1(self):
-        return self.player1_points - self.player2_points == 1
+        return self.almost_one_of_the_players_is_winning() and \
+               self.player1_points - self.player2_points == 1
 
     def advantage_for_player_2(self):
-        return self.player2_points - self.player1_points == 1
+        return self.almost_one_of_the_players_is_winning() and \
+               self.player2_points - self.player1_points == 1
 
     def player1_won(self):
-        return self.player1_points - self.player2_points >= 2
+        return self.almost_one_of_the_players_is_winning() and \
+               self.player1_points - self.player2_points >= 2
 
     def player2_won(self):
         return self.player2_points - self.player1_points >= 2
 
+    def is_love_all(self):
+        return self.player1_points == self.player2_points == 0
+
+    def is_fifteen_all(self):
+        return self.player1_points == self.player2_points == 1
+
+    def is_thirty_all(self):
+        return self.player1_points == self.player2_points == 2
